d20a3e7123536ff845dc8fb2d2677aef398d3349
Tammy <tchen00@stuy.edu>
2019-02-13 11:23:26 -0500

    fix a typo and add in format of lab

--- KnightBoard.java
+++ KnightBoard.java
@@ -7,9 +7,24 @@ public class KnightBoard{
   */
   public KnightBoard(int startingRows, int startingCols){
     if (startingRows <= 0 || startingCols <= 0){
-      return new IllegalArgumentException("cannot have negative row and/or column");
+      throw new IllegalArgumentException("cannot have negative row and/or column");
     }
     board = new int[startingRows][startingCols];
   }
-  
+
+  public String toString(){
+    return "";
+  }
+
+  public boolean solve(int startingRow,int startingCol){
+    return true;
+  }
+
+  public int countSolutions(int startingRow, int startingCol){
+    return 1;
+  }
+
+  private boolean solveH(int row, int col, int level){
+    return true;
+  }
 }
