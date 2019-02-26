a162fa32933f4f1c142d325da9d24ee57a773453
Tammy <tchen00@stuy.edu>
2019-02-25 23:59:17 -0500

    attempt to use Mr. Ks driver TAKE #1

--- KnightBoard.java
+++ KnightBoard.java
@@ -139,8 +139,36 @@ public class KnightBoard{
     return output;
   }
 
+  public static void runTest(int i){
+
+    KnightBoard b;
+    int[]m =   {4,5,5,5,5};
+    int[]n =   {4,5,4,5,5};
+    int[]startx = {0,0,0,1,2};
+    int[]starty = {0,0,0,1,2};
+    int[]answers = {0,304,32,56,64};
+    if(i >= 0 ){
+      try{
+        int correct = answers[i];
+        b = new KnightBoard(m[i%m.length],n[i%m.length]);
+
+        int ans  = b.countSolutions(startx[i],starty[i]);
+
+        if(correct==ans){
+          System.out.println("PASS board size: "+m[i%m.length]+"x"+n[i%m.length]+" "+ans);
+        }else{
+          System.out.println("FAIL board size: "+m[i%m.length]+"x"+n[i%m.length]+" "+ans+" vs "+correct);
+        }
+      }catch(Exception e){
+        System.out.println("FAIL Exception case: "+i);
+
+      }
+    }
+  }
+
   public static void main(String[] args){
-    KnightBoard a = new KnightBoard(10,10);
-    System.out.println(a);
+    for(int i = 0; i < 5; i++){
+      KnightBoard.runTest(i);
+    }
   }
 }
